9bedc6db4e03881862c44a5cea83b6f2fe789b98
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-07 06:58:49 -0700

    Add ability to use multiple nodes

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,17 +1,22 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Graph<T extends Comparable> {
-  ArrayList<T> nodes = new ArrayList<>();
+  List<T> nodes = new ArrayList<>();
 
   public int size() {
     return nodes.size();
   }
 
   public void addNode(T node) {
-      nodes.add(node);
+    nodes.add(node);
   }
 
-  public ArrayList<T> getNodes() {
+  public List<T> getNodes() {
     return nodes;
+  }
+
+  public void addNodes(List<T> nodes) {
+    this.nodes.addAll(nodes);
   }
 }
--- test/GraphTest.java
+++ test/GraphTest.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class GraphTest {
@@ -8,8 +13,7 @@ public class GraphTest {
     Graph<Integer> graph = new Graph<>();
     assertEquals(0, graph.size());
   }
-  
-  // Graph should be able to add a bunch of nodes all at once
+
   // Graph should be able to delete a node
   // Graph should be able to delete a list of nodes
   // Graph should be able to update an edge between two nodes
@@ -20,5 +24,17 @@ public class GraphTest {
     graph.addNode(10);
     assertEquals(1, graph.size());
     assertEquals(new Integer(10), graph.getNodes().get(0));
+  }
+
+  @Test
+  public void testAddNodeList(){
+      List<Integer> nodes = new ArrayList<>();
+      nodes.addAll(Arrays.asList(new Integer[]{20, 9, 0, 2, 1, 0}));
+
+      Graph<Integer> graph = new Graph<>();
+      graph.addNodes(nodes);
+
+      assertEquals(nodes.size(), graph.size());
+      assertEquals(nodes, graph.getNodes());
   }
 }
