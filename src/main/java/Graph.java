a43eba7e3555043efdcd9afbdc3d13281d263073
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-07 09:47:26 -0700

    Convert graph to use generics for edges

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,22 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Graph<T extends Comparable> {
-  List<T> nodes = new ArrayList<>();
+public class Graph<N extends Comparable, E extends Comparable> {
+  private final EdgeReporter<E> edgeReporter;
+  List<N> nodes = new ArrayList<>();
+
+  public Graph(EdgeReporter<E> edgeReporter) {
+    this.edgeReporter = edgeReporter;
+  }
 
   public int size() {
     return nodes.size();
   }
 
-  public void addNode(T node) {
+  public void addNode(N node) {
     nodes.add(node);
   }
 
-  public List<T> getNodes() {
+  public List<N> getNodes() {
     return nodes;
   }
 
-  public void addNodes(List<T> nodes) {
+  public void addNodes(List<N> nodes) {
     this.nodes.addAll(nodes);
+  }
+
+  public E getDisconnectedDistance() {
+    return edgeReporter.disconnectedDistance();
   }
 }
--- test/GraphTest.java
+++ test/GraphTest.java
@@ -7,19 +7,20 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class GraphTest {
+    Double disconnectedDistance = -1.00;
   @Test
-  public void testCreateEmptyGraph() {
-    Graph<Integer> graph = new Graph<>();
+  public void testCreateEmptyGraphUsesDisconnectedValueFromEdgeReporter() {
+    Graph<Integer, Double> graph = new Graph<>(new AdjacencyMatrix(new Double(-1.00)));
     assertEquals(0, graph.size());
+    assertEquals(disconnectedDistance, graph.getDisconnectedDistance());
   }
 
   // Graph should be able to delete a node
   // Graph should be able to delete a list of nodes
   // Graph should be able to update an edge between two nodes
-  // Graph should be able to return a list of nodes within a specified distance from a node
   @Test
   public void testAddNode() {
-    Graph<Integer> graph = new Graph<>();
+    Graph<Integer, Double> graph = new Graph<>(new AdjacencyMatrix(new Double(-1.00)));
     graph.addNode(10);
     assertEquals(1, graph.size());
     assertEquals(new Integer(10), graph.getNodes().get(0));
@@ -30,7 +31,7 @@ public class GraphTest {
     List<Integer> nodes = new ArrayList<>();
     nodes.addAll(Arrays.asList(new Integer[] {20, 9, 0, 2, 1, 0}));
 
-    Graph<Integer> graph = new Graph<>();
+    Graph<Integer, Double> graph = new Graph<>(new AdjacencyMatrix(new Double(-1.00)));
     graph.addNodes(nodes);
 
     assertEquals(nodes.size(), graph.size());
