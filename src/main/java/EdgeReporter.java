3a2d18ed68d056094455d6cca80a94fc3e591b78
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-07 09:31:40 -0700

    Add all methods to EdgeReporter interface

--- src/main/java/EdgeReporter.java
+++ src/main/java/EdgeReporter.java
@@ -1,3 +1,19 @@
 public interface EdgeReporter<T extends Comparable> {
-    public T disconnectedDistance();
+  public T disconnectedDistance();
+
+  public T distanceBetween(int nodeIndex1, int nodeIndex2);
+
+  public void addNode();
+
+  public void createEdge(int nodeIndex1, int nodeIndex2, T distance);
+
+  public void addNodes(int numberToAdd);
+
+  public void deleteNode(int nodeIndex);
+
+  public int size();
+
+  public void deleteEdge(int nodeIndex1, int nodeIndex2);
+
+  public void changeEdge(int nodeIndex1, int nodeIndex2, T newDistance);
 }
