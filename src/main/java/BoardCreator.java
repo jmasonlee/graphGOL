e9479114ddb5cf4824ca9abdca570611d2148042
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-09 17:03:17 -0700

    Extract board creation stuff into Board Creator class

---
+++ src/main/java/BoardCreator.java
@@ -0,0 +1,39 @@
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+
+public class BoardCreator {
+
+    public static String createBoardOutput(List<Cell> cells){
+        StringBuilder emptyBoard = emptyBoardOfSize(getBoardWidth(), 5);
+        return populateBoardWithCells(emptyBoard, cells);
+    }
+
+    private static String populateBoardWithCells(StringBuilder emptyBoard, List<Cell> cells) {
+        StringBuilder populatedBoard = emptyBoard;
+        cells.forEach(c -> {
+            Integer positionInBoard = getCellPositionInBoard(c);
+            populatedBoard.setCharAt(positionInBoard, 'X');
+        });
+        return populatedBoard.toString();
+    }
+
+    private static Integer getCellPositionInBoard(Cell cell) {
+        return (cell.y * getBoardWidth()) + cell.x;
+    }
+
+    private static Integer getBoardWidth() {
+        return 5;
+    }
+
+    private static StringBuilder emptyBoardOfSize(int width, int height) {
+        StringBuilder board = new StringBuilder();
+
+        for (int i = 0; i < height; i++) {
+            board.append(StringUtils.repeat(" ", width));
+            board.append("\n");
+        }
+
+        return board;
+    }
+}
--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -7,40 +7,11 @@ import java.util.List;
 public class GameOfLife {
     private Graph<Cell, Integer> boardGraph = new Graph<Cell, Integer>(new AdjacencyMatrix<>(-1));
 
-    public GameOfLife(List<Cell> cells) {
-        boardGraph.addNodes(cells);
+    public GameOfLife(List<Cell> initialCells) {
+        boardGraph.addNodes(initialCells);
     }
 
     public String outputBoard() {
-        StringBuilder emptyBoard = emptyBoardOfSize(5,5);
-        return populateBoardWithCells(emptyBoard);
-    };
-
-    private String populateBoardWithCells(StringBuilder emptyBoard){
-        StringBuilder populatedBoard = emptyBoard;
-        boardGraph.getNodes().forEach(c -> {
-            Integer positionInBoard = getCellPositionInBoard(c);
-            populatedBoard.setCharAt(positionInBoard, 'X');
-        });
-        return populatedBoard.toString();
-    }
-
-    private Integer getCellPositionInBoard(Cell cell) {
-        return (cell.y * getBoardWidth()) + cell.x;
-    }
-
-    private Integer getBoardWidth(){
-        return 5;
-    }
-
-    private StringBuilder emptyBoardOfSize(int width, int height) {
-        StringBuilder board = new StringBuilder();
-
-        for(int i = 0; i < height; i++) {
-            board.append(StringUtils.repeat(" ", width));
-            board.append("\n");
-        }
-
-        return board;
+        return BoardCreator.createBoardOutput(boardGraph.getNodes());
     }
 }
