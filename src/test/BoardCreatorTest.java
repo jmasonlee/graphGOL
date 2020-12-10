363a1550244d9c953b650147f09c597f56ed246a
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-10 12:23:52 -0700

    Add test to Board Creator

---
+++ src/test/BoardCreatorTest.java
@@ -0,0 +1,28 @@
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.pairwise.Case;
+import org.approvaltests.combinations.pairwise.Pairwise;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class BoardCreatorTest {
+  @Test
+  public void testBoardIsGeneratedWithCellsInCorrectLocation() {
+      List<Integer> coordinateNumbers = Arrays.asList(0, 1, 2, 3, 4);
+      String[] allBoards = new String[25];
+
+
+      for(int x = 0; x < coordinateNumbers.size(); x++){
+          for(int y = 0; y < coordinateNumbers.size(); y++){
+              int index = x*5 + y;
+              String input = String.format("(%d,%d):\n",x,y);
+              allBoards[index] = input + BoardCreator.createBoardOutput(Arrays.asList(new Cell(x,y)));
+          }
+      }
+
+      Approvals.verifyAll("",allBoards);
+  }
+
+}
