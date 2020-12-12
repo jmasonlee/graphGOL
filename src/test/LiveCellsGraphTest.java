506f45c8e0b1f04697a40ce97d85b871f95e18b0
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-12 15:27:10 -0700

    Creathe initial LiveCellsGraphTest & LiveCellsGraph

---
+++ src/test/LiveCellsGraphTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class LiveCellsGraphTest {
+  // For a list of cells > 2 apart from eachother, the graph should not have any edges
+  // For a list of cells that are not neighbours, but <2 apart, the graph should have COPARENT edges
+  // or DISCONNECTED edges
+  // For a list of cells that are neighbours, the graph should have NEIGHBOUR, COPARENT, or
+  // DISCONNECTED edges
+
+  @Test
+  public void name() {}
+
+    //Approvals - verify all combinations of 25 cells?
+}
