d0e121e38d92ee21c04b2a444428e3cc6d7fb97b
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-15 06:30:42 -0700

    Override equality for Cell

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,14 +1,30 @@
 public class Cell implements Comparable {
-    public Integer y;
-    public Integer x;
+  public Integer y;
+  public Integer x;
 
-    public Cell(Integer x, Integer y) {
-        this.x = x;
-        this.y = y;
+  public Cell(Integer x, Integer y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public int compareTo(Object o) {
+    return 0;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (o == this) {
+      return true;
     }
 
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+    if (!(o instanceof Cell)) {
+      return false;
     }
+
+    Cell otherCell = (Cell) o;
+
+    return otherCell.x == this.x && otherCell.y == this.y;
+  }
 }
---
+++ src/test/CellTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class CellTest {
+  @Test
+  public void testEqualityOfTwoCellsWithIdenticalCoordinates() {
+    Cell cell1 = new Cell(1, 4);
+    Cell cell2 = new Cell(1, 4);
+
+    assertEquals(cell1, cell2);
+  }
+
+  @Test
+  public void testEqualityOfCellComparedWithItself() {
+    Cell cell1 = new Cell(1, 4);
+
+    assertEquals(cell1, cell1);
+  }
+
+  @Test
+  public void testNotEqualToNotACell() {
+    Cell cell1 = new Cell(1, 4);
+
+    assertNotEquals(cell1, "Hello!");
+  }
+}
