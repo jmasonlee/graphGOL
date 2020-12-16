4effe721255a888786a403b2e5668f0c5a6b2c0f
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-16 07:03:56 -0700

    Add maximum distance to CellRelationships

--- src/main/java/CellRelationships.java
+++ src/main/java/CellRelationships.java
@@ -1,3 +1,9 @@
 public enum CellRelationships {
-    DISCONNECTED, NEIGHBOUR, COPARENT
+    DISCONNECTED(-1), SELF(0), NEIGHBOUR(1), COPARENT(2);
+
+    public final int maxDistance;
+
+    private CellRelationships(int maxDistance){
+        this.maxDistance = maxDistance;
+    }
 }
