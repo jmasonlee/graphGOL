c2147160bfa619e89c1886e72723e7315f668a65
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-17 06:51:46 -0700

    Tested RelationshipClassifier via unit test

--- src/main/java/cell/RelationshipClassifier.java
+++ src/main/java/cell/RelationshipClassifier.java
@@ -1,17 +1,39 @@
 package cell;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.groupingBy;
+
 public class RelationshipClassifier {
   public static Map<Relationships, List<Cell>> classify(Cell cell, List<Cell> otherCells) {
-    Map<Relationships, List<Cell>> relationshipsMap = new HashMap<>();
-    otherCells.forEach(otherCell -> {
-        relationshipsMap.put(Relationships.SELF, Arrays.asList(new Cell[]{otherCell}));
-    });
+    Map<Relationships, List<Cell>> relationshipsMap =
+        otherCells
+            .stream()
+            .collect(
+                groupingBy(
+                    otherCell -> {
+                      Double distanceBetweenCells =
+                          calculateEuclideanDistanceBetweenCells(cell, (Cell) otherCell);
+                      if (distanceBetweenCells == Relationships.SELF.maxDistance) {
+                        return Relationships.SELF;
+                      } else if (distanceBetweenCells <= Relationships.NEIGHBOUR.maxDistance) {
+                        return Relationships.NEIGHBOUR;
+                      } else if (distanceBetweenCells <= Relationships.COPARENT.maxDistance) {
+                        return Relationships.COPARENT;
+                      } else {
+                        return Relationships.DISCONNECTED;
+                      }
+                    }));
+
     return relationshipsMap;
+  }
+
+  private static Double calculateEuclideanDistanceBetweenCells(Cell cell1, Cell cell2) {
+    Integer xDistance = Math.abs(cell1.x - cell2.x);
+    Integer yDistance = Math.abs(cell1.y - cell2.y);
+
+    return Math.hypot(xDistance, yDistance);
   }
 
   public enum Relationships {
--- src/test/cell/RelationshipClassifierTest.java
+++ src/test/cell/RelationshipClassifierTest.java
@@ -8,14 +8,26 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.Assert.*;
+
 public class RelationshipClassifierTest {
   @Test
-  public void testCellIsClassification() {
+  public void testCellClassification() {
     List<Cell> cellsToClassify = Arrays.asList(new Cell[]{new Cell(1,2)});
 
     Map<RelationshipClassifier.Relationships, List<Cell>> relationship =
         RelationshipClassifier.classify(new Cell(1, 2), cellsToClassify);
 
     Approvals.verify(relationship);
+  }
+
+  @Test
+  public void testNeighbourRelationship() {
+    List<Cell> cellsToClassify = Arrays.asList(new Cell[]{new Cell(1,1)});
+
+    Map<RelationshipClassifier.Relationships, List<Cell>> relationship =
+        RelationshipClassifier.classify(new Cell(1, 2), cellsToClassify);
+
+    assertEquals(new Cell(1,1), relationship.get(RelationshipClassifier.Relationships.NEIGHBOUR).get(0));
   }
 }
