108cb521e837d0949e31fe4fbf3c2a314bd1f11e
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-17 05:54:25 -0700

    Format RelationshipClassifier

--- src/main/java/cell/RelationshipClassifier.java
+++ src/main/java/cell/RelationshipClassifier.java
@@ -4,21 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RelationshipClassifier {
-    public static Map<Relationships, Cell> classify(Cell cell, Cell otherCell) {
-        Map<Relationships, Cell> relationshipsMap = new HashMap<Relationships, Cell>();
-        relationshipsMap.put(Relationships.SELF, otherCell);
-        return relationshipsMap;
+  public static Map<Relationships, Cell> classify(Cell cell, Cell otherCell) {
+    Map<Relationships, Cell> relationshipsMap = new HashMap<Relationships, Cell>();
+    relationshipsMap.put(Relationships.SELF, otherCell);
+    return relationshipsMap;
+  }
+
+  public enum Relationships {
+    DISCONNECTED(-1),
+    SELF(0),
+    NEIGHBOUR(1),
+    COPARENT(2);
+
+    public final int maxDistance;
+
+    private Relationships(int maxDistance) {
+      this.maxDistance = maxDistance;
     }
-
-    public enum Relationships {
-        DISCONNECTED(-1), SELF(0), NEIGHBOUR(1), COPARENT(2);
-
-        public final int maxDistance;
-
-        private Relationships(int maxDistance){
-            this.maxDistance = maxDistance;
-        }
-    }
-
-
+  }
 }
