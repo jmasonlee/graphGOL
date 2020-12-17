d648592f6fbc247a26afbc5b98d8692e5d4a6857
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-17 05:53:43 -0700

    Format RelationshipClassifierTest

--- src/test/cell/RelationshipClassifierTest.java
+++ src/test/cell/RelationshipClassifierTest.java
@@ -1,22 +1,16 @@
 package cell;
 
-import cell.Cell;
-import cell.RelationshipClassifier;
 import org.approvaltests.Approvals;
-import org.approvaltests.strings.Printable;
 import org.junit.Test;
 
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 public class RelationshipClassifierTest {
   @Test
   public void testCellIsClassifiedAsSelf() {
-    Map<RelationshipClassifier.Relationships, Cell> relationship=
+    Map<RelationshipClassifier.Relationships, Cell> relationship =
         RelationshipClassifier.classify(new Cell(1, 2), new Cell(1, 2));
 
     Approvals.verify(relationship);
   }
-
 }
