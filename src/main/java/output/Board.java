65b6de6a63624a66a7f5f348b15232f082733ef8
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-19 09:03:16 -0700

    Add ability for creating empty board

--- src/main/java/output/Board.java
+++ src/main/java/output/Board.java
@@ -1,19 +1,22 @@
 package output;
 
 import cell.Cell;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Board {
-  private int upperYValue;
-  private int lowerYValue;
-  private int leftmostXValue;
-  private int rightmostXValue;
+  private StringBuilder board;
+  private int upperYValue = 5;
+  private int lowerYValue = 0;
+  private int leftmostXValue = 0;
+  private int rightmostXValue = 5;
 
   public Board(List<Cell> cellsForBoard) {
     calculateBoardBounds(cellsForBoard);
+    createEmptyBoard();
   }
 
   public int getWidth() {
@@ -24,11 +27,30 @@ public class Board {
     return Math.abs(upperYValue - lowerYValue);
   }
 
+  @Override
+  public String toString() {
+    return board.toString();
+  }
+
+  private void createEmptyBoard() {
+    board = new StringBuilder();
+
+    for (int i = 0; i < getHeight(); i++) {
+      board.append(StringUtils.repeat(" ", getWidth()));
+      if (i < getHeight() - 1) {
+        board.append("\n");
+      }
+    }
+  }
+
   private void calculateBoardBounds(List<Cell> cellsForBoard) {
+    if (cellsForBoard.isEmpty()) {
+      return;
+    }
+
     upperYValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
     lowerYValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
     leftmostXValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
     rightmostXValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
   }
-
 }
--- src/test/output/BoardTest.java
+++ src/test/output/BoardTest.java
@@ -8,13 +8,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class BoardTest {
   @Test
-  public void testCreateBoard(){
-      List<Cell> cellsForBoard = Arrays.asList(new Cell[]{ new Cell(-4,-3), new Cell(-1, 0), new Cell(2,6)});
-      Board board = new Board(cellsForBoard);
-      Approvals.verify("height:"+board.getHeight()+" width:"+board.getWidth());
+  public void testCreateBoard() {
+    List<Cell> cellsForBoard =
+        Arrays.asList(new Cell[] {new Cell(-4, -3), new Cell(-1, 0), new Cell(2, 6)});
+    Board board = new Board(cellsForBoard);
+    Approvals.verify("height:" + board.getHeight() + " width:" + board.getWidth());
+  }
+
+  @Test
+  public void testCreateEmptyBoard() {
+    List<Cell> cells = new ArrayList<>();
+    Board board = new Board(cells);
+    assertEquals("     \n     \n     \n     \n     ", board.toString());
   }
 }
