4af02e2c383df5327223e535cdffc872ac59781f
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-19 08:35:45 -0700

    Combine Board height and width calculations
    Makes getting an upper left coordinate easier

--- src/main/java/output/Board.java
+++ src/main/java/output/Board.java
@@ -2,44 +2,34 @@ package output;
 
 import cell.Cell;
 
-import javax.swing.*;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Board {
-    private int width;
+  private int width;
+  private int height;
 
-    public int getWidth() {
-        return width;
-    }
+  public Board(List<Cell> cellsForBoard) {
+    calculateBoardBounds(cellsForBoard);
+  }
 
-    public int getHeight() {
-        return height;
-    }
+  public int getWidth() {
+    return width;
+  }
 
-    private int height;
+  public int getHeight() {
+    return height;
+  }
 
-    public Board(List<Cell> cellsForBoard) {
-        calculateBoardDimensions(cellsForBoard);
-    }
+  private void calculateBoardBounds(List<Cell> cellsForBoard) {
+    int upperYValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
+    int lowerYValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
+    int leftmostXValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
+    int rightmostXValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
 
-    private void calculateBoardDimensions(List<Cell> cellsForBoard){
-        this.width = calculateBoardWidth(cellsForBoard);
-        this.height = calculateBoardHeight(cellsForBoard);
-    }
+    this.width = Math.abs(leftmostXValue - rightmostXValue);
+    this.height =  Math.abs(upperYValue - lowerYValue);
+  }
 
-    private int calculateBoardHeight(List<Cell> cells) {
-        int upperYValue = Collections.min(cells, Comparator.comparing(cell -> cell.x)).x;
-        int lowerYValue = Collections.max(cells, Comparator.comparing(cell -> cell.x)).x;
-
-        return Math.abs(upperYValue - lowerYValue);
-    }
-
-    private int calculateBoardWidth(List<Cell> cells){
-        int leftmostXValue = Collections.min(cells, Comparator.comparing(cell -> cell.y)).y;
-        int rightmostXValue = Collections.max(cells, Comparator.comparing(cell -> cell.y)).y;
-
-        return Math.abs(leftmostXValue - rightmostXValue);
-    }
 }
