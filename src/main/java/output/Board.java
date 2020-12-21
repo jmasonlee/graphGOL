800e8eeb85a304c188a664a74585fb35b1df3276
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-21 06:25:53 -0700

    Redo Board class tests

--- src/main/java/output/Board.java
+++ src/main/java/output/Board.java
@@ -10,37 +10,25 @@ import java.util.List;
 public class Board {
   private StringBuilder board;
   private int upperYValue = 5;
-  private int lowerYValue = 0;
   private int leftmostXValue = 0;
-  private int rightmostXValue = 5;
+  private int height = 5;
+  private int width = 5;
 
   public Board(List<Cell> cellsForBoard) {
     calculateBoardBounds(cellsForBoard);
-    createEmptyBoard();
   }
 
   public int getWidth() {
-    return Math.abs(leftmostXValue - rightmostXValue);
+    return width;
   }
 
   public int getHeight() {
-    return Math.abs(upperYValue - lowerYValue);
+    return height;
   }
 
   @Override
   public String toString() {
     return board.toString();
-  }
-
-  private void createEmptyBoard() {
-    board = new StringBuilder();
-
-    for (int i = 0; i < getHeight(); i++) {
-      board.append(StringUtils.repeat(" ", getWidth()));
-      if (i < getHeight() - 1) {
-        board.append("\n");
-      }
-    }
   }
 
   private void calculateBoardBounds(List<Cell> cellsForBoard) {
@@ -49,8 +37,11 @@ public class Board {
     }
 
     upperYValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
-    lowerYValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
+    int lowerYValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.y)).y;
+    height = Math.abs(upperYValue - lowerYValue);
+
     leftmostXValue = Collections.min(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
-    rightmostXValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
+    int rightmostXValue = Collections.max(cellsForBoard, Comparator.comparing(cell -> cell.x)).x;
+    width =  Math.abs(leftmostXValue - rightmostXValue);
   }
 }
--- src/test/output/BoardTest.java
+++ src/test/output/BoardTest.java
@@ -13,16 +13,7 @@ import static org.junit.Assert.assertEquals;
 public class BoardTest {
   @Test
   public void testCreateBoard() {
-    List<Cell> cellsForBoard =
-        Arrays.asList(new Cell[] {new Cell(-4, -3), new Cell(-1, 0), new Cell(2, 6)});
-    Board board = new Board(cellsForBoard);
+    Board board = new Board(new ArrayList<>());
     Approvals.verify("height:" + board.getHeight() + " width:" + board.getWidth());
-  }
-
-  @Test
-  public void testCreateEmptyBoard() {
-    List<Cell> cells = new ArrayList<>();
-    Board board = new Board(cells);
-    assertEquals("     \n     \n     \n     \n     ", board.toString());
   }
 }
