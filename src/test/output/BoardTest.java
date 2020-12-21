f32fea55bef77620dca71dd69224e14441ce4767
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-21 07:03:13 -0700

    Roll Negative Space test into approval

--- src/test/output/BoardTest.java
+++ src/test/output/BoardTest.java
@@ -13,34 +13,23 @@ import static org.junit.Assert.assertEquals;
 public class BoardTest {
   @Test
   public void testCreateBoard() {
-    List<Cell> cellsToBeTested = Arrays.asList(new Cell [] {
-        new Cell(1,4)
-    });
+    List<Cell[]> cellsToBeTested = new ArrayList<>();
+    cellsToBeTested.add(new Cell[]{});
+    cellsToBeTested.add(new Cell[]{new Cell(1,4)});
+    cellsToBeTested.add(new Cell[]{new Cell( -1, -4)});
 
-    List<Cell> cellsUnderTest = new ArrayList<>();
     List<String> testResults = new ArrayList<>();
 
-    for(int i = 0; i <= cellsToBeTested.size(); i++){
-      Board board = new Board(cellsUnderTest);
-      testResults.add("inputCells: "+ cellsUnderTest +
+    for(int i = 0; i < cellsToBeTested.size(); i++){
+      List<Cell> cellList = Arrays.asList(cellsToBeTested.get(i));
+      Board board = new Board(cellList);
+      testResults.add("inputCells: "+ cellList +
           "\n\t Height:" + board.getHeight() +
           "\n\t Width:" + board.getWidth() +
           "\n\t Upper Left Coordinates: (" + board.getLeftmostXValue() + ", "+ board.getUpperYValue()+")");
-
-      if(i < cellsToBeTested.size()){
-        cellsUnderTest.add(cellsToBeTested.get(i));
-      }
     }
 
     Approvals.verifyAll("", testResults);
   }
-
-  @Test
-  public void testCreateBoardWithHighestCellsInNegativeSpace(){
-    Board board = new Board(Arrays.asList(new Cell[]{new Cell(-1,-4)}));
-    assertEquals(5, board.getHeight());
-    assertEquals(5, board.getWidth());
-    assertEquals(-1, board.getLeftmostXValue());
-    assertEquals(-4, board.getUpperYValue());
-  }
+  
 }
