f6a21087c7987e3bc276ce3903e8e891bc4787cb
Jacqueline Bilston <jmasonlee@gmail.com>
2020-12-23 17:02:51 -0700

    Output RelationshipClassifier test as board
    I didn't like the way my map was outputting to my
    approved file, because there wasn't a way to see
    if it was correct at a glance. I came up with the
    idea that it should output as a board string, not
    a map output. That required me to make changes
    to the existing BoardOutputter to enable it to
    create boards in negative board space and create
    boards that were larger than 5x5. I needed to make
    the changes anyway in order for this to be a
    complete GOL solution.
    
    This commit is me modifying the current "SELF"
    classification test to use the new BoardOutputter.

--- src/test/cell/RelationshipClassifierTest.java
+++ src/test/cell/RelationshipClassifierTest.java
@@ -2,32 +2,45 @@ package cell;
 
 import org.approvaltests.Approvals;
 import org.junit.Test;
+import output.BoardOutputter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class RelationshipClassifierTest {
   @Test
   public void testCellClassification() {
-    List<Cell> cellsToClassify = Arrays.asList(new Cell[]{new Cell(1,2)});
+    List<Cell> cellsToClassify = getListOfCellsToClassify(new Cell(0, 0));
 
     Map<RelationshipClassifier.Relationships, List<Cell>> relationship =
-        RelationshipClassifier.classify(new Cell(1, 2), cellsToClassify);
+        RelationshipClassifier.classify(new Cell(0, 0), cellsToClassify);
 
-    Approvals.verify(relationship);
+    List<String> relationshipBoards = new ArrayList<>();
+    relationshipBoards.add("\n" + BoardOutputter.createBoardOutput(relationship.get(RelationshipClassifier.Relationships.SELF)));
+    Approvals.verifyAll("Boards", relationshipBoards);
   }
 
   @Test
   public void testNeighbourRelationship() {
-    List<Cell> cellsToClassify = Arrays.asList(new Cell[]{new Cell(1,1)});
+    List<Cell> cellsToClassify = Arrays.asList(new Cell[]{new Cell(1, 1)});
 
     Map<RelationshipClassifier.Relationships, List<Cell>> relationship =
         RelationshipClassifier.classify(new Cell(1, 2), cellsToClassify);
 
-    assertEquals(new Cell(1,1), relationship.get(RelationshipClassifier.Relationships.NEIGHBOUR).get(0));
+    assertEquals(new Cell(1, 1), relationship.get(RelationshipClassifier.Relationships.NEIGHBOUR).get(0));
+  }
+
+  private List<Cell> getListOfCellsToClassify(Cell cell) {
+    List<Cell> cellsToClassify = new ArrayList<>();
+    for (int x = cell.x - 3; x <= cell.x + 3; x++) {
+      for (int y = cell.y - 3; y <= cell.y + 3; y++) {
+        cellsToClassify.add(new Cell(x, y));
+      }
+    }
+    return cellsToClassify;
   }
 }
