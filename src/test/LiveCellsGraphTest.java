7d8b93bbaf2a84fe48ace2a68c10a4adb59b17d1
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-24 06:58:11 -0700

    Remove comments from LiveCellsGraphTest

--- src/test/LiveCellsGraphTest.java
+++ src/test/LiveCellsGraphTest.java
@@ -8,12 +8,6 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class LiveCellsGraphTest {
-  // For a list of cells > 2 apart from eachother, the graph should not have any edges
-  // For a list of cells that are not neighbours, but <2 apart, the graph should have COPARENT edges
-  // or DISCONNECTED edges
-  // For a list of cells that are neighbours, the graph should have NEIGHBOUR, COPARENT, or
-  // DISCONNECTED edges
-  // Should be able to retrieve a list of cells to live for the next turn
 
   @Test
   public void testCanBeCreatedWithArrayOfCells() {
