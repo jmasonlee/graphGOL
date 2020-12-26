cf7d5d85300ac2a32e14c6dcb85fe937015158bf
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-26 08:49:42 -0700

    Expand one-cell test and roll into approvals
    In retrosect, the expansion & rolling should have
    been two different steps. Doing them together made
     this harder than it needed to be.

--- src/test/gameOfLife/LiveCellsGraphTest.java
+++ src/test/gameOfLife/LiveCellsGraphTest.java
@@ -1,28 +1,48 @@
 package gameOfLife;
 
 import gameOfLife.cell.Cell;
+import gameOfLife.output.BoardOutputter;
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 public class LiveCellsGraphTest {
   @Test
   public void testCanGetCellsWithSpecifiedNumberOfNeighbours() {
+    Integer[] neighbourRange = new Integer[] {- 1,0,1};
 
+    List<Cell> cells = new ArrayList<>();
+    StringBuilder testOutput = new StringBuilder("\n");
+
+    for(int x = 0; x < neighbourRange.length; x++){
+      for(int y = 0; y < neighbourRange.length; y++) {
+        if(neighbourRange[x] == 0 && neighbourRange[y] == 0){
+          continue;
+        }
+
+        Cell centre = calculateCentreBasedOnXandY(x,y);
+        cells.add(centre);
+        cells.add(new Cell(centre.x+neighbourRange[x], centre.y+neighbourRange[y]));
+      }
+    }
+
+    LiveCellsGraph graph = new LiveCellsGraph(cells);
+    testOutput.append(BoardOutputter.createBoardOutput(cells));
+    testOutput.append("\nCells With 1 neighbour:\n");
+    testOutput.append(BoardOutputter.createBoardOutput(graph.getCellsWithNumberOfNeighbours(1)));
+    Approvals.verify(testOutput);
   }
 
-  @Test
-  public void testCanGetCellsWithOneNeighbour() {
-    List<Cell> cells = new ArrayList<>();
-    cells.add(new Cell(0, 0));
-    cells.add(new Cell(0, 1));
+  private Cell calculateCentreBasedOnXandY(Integer x, Integer y) {
+    int initialX = 0;
+    int centreSeparator = 6;
+    int gridNumber = x*3 + y;
 
-    LiveCellsGraph liveCells = new LiveCellsGraph(cells);
-    assertEquals(cells, liveCells.getCellsWithNumberOfNeighbours(1));
+    return new Cell(initialX + (centreSeparator*gridNumber),0);
   }
 
   @Test
@@ -38,6 +58,4 @@ public class LiveCellsGraphTest {
     LiveCellsGraph liveCells = new LiveCellsGraph(cells);
     assertEquals(expectedOutput, liveCells.getCellsWithNumberOfNeighbours(2));
   }
-
-  // Approvals - how
 }
