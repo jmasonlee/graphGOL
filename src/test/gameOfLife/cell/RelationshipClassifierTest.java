524961346f3510887701a0ca8af1164cf30640bc
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-29 08:50:48 -0700

    Extract method to calculate relative dimensions

--- src/test/gameOfLife/cell/RelationshipClassifierTest.java
+++ src/test/gameOfLife/cell/RelationshipClassifierTest.java
@@ -1,9 +1,9 @@
 package gameOfLife.cell;
 
 import gameOfLife.TestUtils.CellCoverage;
+import gameOfLife.output.BoardOutputter;
 import org.approvaltests.Approvals;
 import org.junit.Test;
-import gameOfLife.output.BoardOutputter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,27 +16,42 @@ public class RelationshipClassifierTest {
     List<Cell> cellsToClassify = getListOfCellsToClassify(new Cell(0, 0));
 
     Map<Relationships, List<Cell>> relationship =
-      RelationshipClassifier.classify(new Cell(0, 0), cellsToClassify).getRelationships();
+        RelationshipClassifier.classify(new Cell(0, 0), cellsToClassify).getRelationships();
 
     List<String> relationshipBoards = new ArrayList<>();
-    relationshipBoards.add("\n"
-        + BoardOutputter.createBoardOutput(relationship.get(Relationships.SELF)));
-    relationshipBoards.add("\n"
-        + BoardOutputter.createBoardOutput(relationship.get(Relationships.NEIGHBOUR)));
-    relationshipBoards.add("\n"
-        + BoardOutputter.createBoardOutput(relationship.get(Relationships.COPARENT)));
-    relationshipBoards.add("\n"
-        + BoardOutputter.createBoardOutput(relationship.get(Relationships.DISCONNECTED)));
+    relationshipBoards.add(
+        "\n" + BoardOutputter.createBoardOutput(relationship.get(Relationships.SELF)));
+    relationshipBoards.add(
+        "\n" + BoardOutputter.createBoardOutput(relationship.get(Relationships.NEIGHBOUR)));
+    relationshipBoards.add(
+        "\n" + BoardOutputter.createBoardOutput(relationship.get(Relationships.COPARENT)));
+    relationshipBoards.add(
+        "\n" + BoardOutputter.createBoardOutput(relationship.get(Relationships.DISCONNECTED)));
     Approvals.verifyAll("Boards", relationshipBoards);
   }
 
   public List<Cell> getListOfCellsToClassify(Cell cell) {
     int maxOneDimensionalDistance = 3;
 
-    Cell lowerLeftCell = new Cell(cell.x - maxOneDimensionalDistance, cell.y - maxOneDimensionalDistance);
-    Cell upperRightCell = new Cell(cell.x  + maxOneDimensionalDistance + 1, cell.y + maxOneDimensionalDistance + 1);
-    List<Cell> cellsToClassify = CellCoverage.generateAllPossibleCellsBetweenTwoCells(lowerLeftCell, upperRightCell);
+    Cell lowerLeftCell =
+        new Cell(
+            relativeLowerLeftLocaltion(maxOneDimensionalDistance, cell.x),
+            relativeLowerLeftLocaltion(maxOneDimensionalDistance, cell.y));
+    Cell upperRightCell =
+        new Cell(
+            relativeUpperRightLocation(maxOneDimensionalDistance, cell.x),
+            relativeUpperRightLocation(maxOneDimensionalDistance, cell.y));
+    List<Cell> cellsToClassify =
+        CellCoverage.generateAllPossibleCellsBetweenTwoCells(lowerLeftCell, upperRightCell);
 
     return cellsToClassify;
+  }
+
+  private int relativeUpperRightLocation(int maxOneDimensionalDistance, Integer centralLocation) {
+    return centralLocation + maxOneDimensionalDistance + 1;
+  }
+
+  private int relativeLowerLeftLocaltion(int maxOneDimensionalDistance, Integer centralLocation) {
+    return centralLocation - maxOneDimensionalDistance;
   }
 }
