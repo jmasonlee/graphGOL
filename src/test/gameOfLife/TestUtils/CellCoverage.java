ef3e9ccb8c6a47e793c207e790a3c4239910c716
Jacqueline Lee <jmasonlee@gmail.com>
2020-12-29 09:32:45 -0700

    Fix bug in CellCoverage - should be inclusive
    I found this because my test cases that used this
    utility function weren't returning what I expected
    after using this function. Do utility test cases
    need to be tested? I still feel like no.... but
    maybe?

--- src/test/gameOfLife/TestUtils/CellCoverage.java
+++ src/test/gameOfLife/TestUtils/CellCoverage.java
@@ -9,8 +9,8 @@ public class CellCoverage {
   public static List<Cell> generateAllPossibleCellsBetweenTwoCells(Cell lowerLeftCell, Cell upperRightCell) {
     List<Cell> cells = new ArrayList<>();
 
-    for (int x = lowerLeftCell.x; x < upperRightCell.x; x++) {
-      for (int y = lowerLeftCell.y; y < upperRightCell.y; y++) {
+    for (int x = lowerLeftCell.x; x <= upperRightCell.x; x++) {
+      for (int y = lowerLeftCell.y; y <= upperRightCell.y; y++) {
         cells.add(new Cell(x,y));
       }
     }
