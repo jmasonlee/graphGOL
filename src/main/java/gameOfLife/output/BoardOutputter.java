dbc80163d6db9e63f359d226044bc42a1ffc9fed
Jacqueline Lee <jmasonlee@gmail.com>
2021-01-02 08:14:25 -0700

    List Y coordinates on board

--- src/main/java/gameOfLife/output/BoardOutputter.java
+++ src/main/java/gameOfLife/output/BoardOutputter.java
@@ -3,10 +3,13 @@ package gameOfLife.output;
 import gameOfLife.cell.Cell;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BoardOutputter {
 
@@ -14,11 +17,11 @@ public class BoardOutputter {
   private static final String TOP_BOUNDARY_END = " \n";
   private static final String CELL = "|__";
   private static final String ROW_END = "|\n";
-  private static final int SHIFT = 1;
+  private static final int SHIFT = 3;
 
   public static String createBoardOutput(List<Cell> cells) {
     BoardBounds boardBounds = new BoardBounds(cells);
-    StringBuilder emptyBoard = emptyBoardOfSize(boardBounds.getWidth(), boardBounds.getHeight());
+    StringBuilder emptyBoard = emptyBoardOfSize(boardBounds);
     return populateBoardWithCells(emptyBoard, cells, boardBounds);
   }
 
@@ -34,23 +37,37 @@ public class BoardOutputter {
     int adjustedX = Math.abs(boardBounds.getLeftmostXValue() - cell.x);
     int adjustedY = Math.abs(boardBounds.getUpperYValue() - cell.y);
 
-    int boardWidth = (CELL.length() * boardBounds.getWidth()) + ROW_END.length();
+    int boardWidth = 2 + (CELL.length() * boardBounds.getWidth()) + ROW_END.length();
 
     return (boardWidth + SHIFT) + (adjustedY * boardWidth) + (adjustedX * CELL.length());
   }
 
-  private static StringBuilder emptyBoardOfSize(int width, int height) {
+  private static StringBuilder emptyBoardOfSize(BoardBounds boardBounds) {
     StringBuilder board = new StringBuilder();
+    List<String> yCoords = createCoordinates(boardBounds.upperYValue, boardBounds.height);
 
-    board.append(StringUtils.repeat(TOP_BOUNDARY, width));
+    board.append("  ");
+    board.append(StringUtils.repeat(TOP_BOUNDARY, boardBounds.width));
     board.append(TOP_BOUNDARY_END);
 
-    for (int i = 0; i < height; i++) {
-      board.append(StringUtils.repeat(CELL, width));
+    for (int i = 0; i < boardBounds.height; i++) {
+      board.append(yCoords.get(i));
+      board.append(StringUtils.repeat(CELL, boardBounds.width));
       board.append(ROW_END);
     }
 
     return board;
+  }
+
+  private static List<String> createCoordinates(int startAt, int dimensionSize) {
+    Integer endAt = startAt - dimensionSize + 1;
+    List<String> coordinateNumbers = IntStream.rangeClosed(endAt, startAt).boxed()
+      .map(coord -> coord < 0 ? coord.toString() : " " + coord.toString())
+      .collect(Collectors.toList());
+
+    Collections.reverse(coordinateNumbers);
+
+    return coordinateNumbers;
   }
 
   public static class BoardBounds {
