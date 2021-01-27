fa39be10313d60242567269cd509e1b8813b74c0
Jacqueline Lee <jmasonlee@gmail.com>
2021-01-27 06:17:09 -0700

    Extract BoardDrawer class

---
+++ src/main/java/gameOfLife/output/BoardDrawer.java
@@ -0,0 +1,55 @@
+package gameOfLife.output;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BoardDrawer {
+  static final String TOP_BOUNDARY_END = " \n";
+  static final String CELL = "|__";
+  static final String ROW_END = "|\n";
+
+  static List<String> emptyBoardOfSize(BoardBounds boardBounds) {
+    List<String> board = new ArrayList<String>();
+
+    Coordinates coordinates = new Coordinates(boardBounds);
+
+    board.addAll(createHeaderRow(coordinates.xCoordinates, coordinates.widestCoordinateString));
+    board.addAll(createEmptyRows(boardBounds, coordinates.yCoordinates));
+
+    return board;
+  }
+
+  static List<String> createHeaderRow(List<String> xCoords, Integer widestY) {
+    List<String> board = new ArrayList<String>();
+
+    board.add(" " + String.join("", Collections.nCopies(widestY, " ")));
+    board.addAll(formatIndividualColumnHeaders(xCoords));
+    board.add(TOP_BOUNDARY_END);
+
+    return board;
+  }
+
+  static List<String> formatIndividualColumnHeaders(List<String> xCoords) {
+    List<String> formattedCoords = new ArrayList<String>();
+    formattedCoords.add(xCoords.get(0));
+
+    for (int i = 1; i < xCoords.size(); i++) {
+      formattedCoords.add(" " + xCoords.get(i));
+    }
+
+    return formattedCoords;
+  }
+
+  static List<String> createEmptyRows(BoardBounds boardBounds, List<String> yCoords) {
+    List<String> board = new ArrayList<String>();
+
+    for (int i = 0; i < boardBounds.getHeight(); i++) {
+      board.add(yCoords.get(i));
+      board.addAll(Collections.nCopies(boardBounds.getWidth(), CELL));
+      board.add(ROW_END);
+    }
+
+    return board;
+  }
+}
--- src/main/java/gameOfLife/output/BoardOutputter.java
+++ src/main/java/gameOfLife/output/BoardOutputter.java
@@ -2,19 +2,13 @@ package gameOfLife.output;
 
 import gameOfLife.cell.Cell;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class BoardOutputter {
 
-  private static final String TOP_BOUNDARY_END = " \n";
-  private static final String CELL = "|__";
-  private static final String ROW_END = "|\n";
-
   public static String createBoardOutput(List<Cell> cells) {
     BoardBounds boardBounds = new BoardBounds(cells);
-    List<String> emptyBoard = emptyBoardOfSize(boardBounds);
+    List<String> emptyBoard = BoardDrawer.emptyBoardOfSize(boardBounds);
     return String.join("", populateBoardWithCells(emptyBoard, cells, boardBounds));
   }
 
@@ -35,50 +29,6 @@ public class BoardOutputter {
     int boardWidth = 2 + boardBounds.getWidth();
 
     return boardWidth + (adjustedY * boardWidth) + (adjustedX + 1);
-  }
-
-  private static List<String> emptyBoardOfSize(BoardBounds boardBounds) {
-    List<String> board = new ArrayList<>();
-
-    Coordinates coordinates = new Coordinates(boardBounds);
-
-    board.addAll(createHeaderRow(coordinates.xCoordinates, coordinates.widestCoordinateString));
-    board.addAll(createEmptyRows(boardBounds, coordinates.yCoordinates));
-
-    return board;
-  }
-
-  private static List<String> createHeaderRow(List<String> xCoords, Integer widestY) {
-    List<String > board = new ArrayList<>();
-
-    board.add(" " + String.join("", Collections.nCopies(widestY, " ")));
-    board.addAll(formatIndividualColumnHeaders(xCoords));
-    board.add(TOP_BOUNDARY_END);
-
-    return board;
-  }
-
-  private static List<String> formatIndividualColumnHeaders(List<String> xCoords) {
-    List<String> formattedCoords = new ArrayList<>();
-    formattedCoords.add(xCoords.get(0));
-
-    for (int i = 1; i < xCoords.size(); i++) {
-      formattedCoords.add(" "+xCoords.get(i));
-    }
-
-    return formattedCoords;
-  }
-
-  private static List<String> createEmptyRows(BoardBounds boardBounds, List<String> yCoords) {
-    List<String> board = new ArrayList<>();
-
-    for (int i = 0; i < boardBounds.getHeight(); i++) {
-      board.add(yCoords.get(i));
-      board.addAll(Collections.nCopies(boardBounds.getWidth(), CELL));
-      board.add(ROW_END);
-    }
-
-    return board;
   }
 
 }
