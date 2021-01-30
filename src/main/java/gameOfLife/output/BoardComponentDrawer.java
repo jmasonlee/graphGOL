cb776c5cabcee286fab083913e3bcad58184bf22
Jacqueline Lee <jmasonlee@gmail.com>
2021-01-30 06:44:58 -0700

    Move populateBoardWithCells to BoardComponentDrawer

--- src/main/java/gameOfLife/output/BoardComponentDrawer.java
+++ src/main/java/gameOfLife/output/BoardComponentDrawer.java
@@ -1,5 +1,7 @@
 package gameOfLife.output;
 
+import gameOfLife.cell.Cell;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -51,5 +53,24 @@ public class BoardComponentDrawer {
         });
 
     return board;
+  }
+
+  static List<String> populateBoardWithCells(List<String> oldBoard, List<Cell> cells, BoardBounds boardBounds) {
+    List<String> boardWithCells = oldBoard;
+    cells.forEach(c -> {
+      int positionInBoard = getCellPositionInBoard(c, boardBounds);
+      boardWithCells.set(positionInBoard, "|X_");
+    });
+
+    return boardWithCells;
+  }
+
+  private static int getCellPositionInBoard(Cell cell, BoardBounds boardBounds) {
+    int adjustedX = Math.abs(boardBounds.getLeftmostXValue() - cell.x);
+    int adjustedY = Math.abs(boardBounds.getUpperYValue() - cell.y);
+
+    int boardWidth = 2 + boardBounds.getWidth();
+
+    return boardWidth + (adjustedY * boardWidth) + (adjustedX + 1);
   }
 }
--- src/main/java/gameOfLife/output/BoardOutputter.java
+++ src/main/java/gameOfLife/output/BoardOutputter.java
@@ -9,26 +9,7 @@ public class BoardOutputter {
   public static String createBoardOutput(List<Cell> cells) {
     BoardBounds boardBounds = new BoardBounds(cells);
     List<String> emptyBoard = BoardComponentDrawer.emptyBoardOfSize(boardBounds);
-    return String.join("", populateBoardWithCells(emptyBoard, cells, boardBounds));
-  }
-
-  private static List<String> populateBoardWithCells(List<String> oldBoard, List<Cell> cells, BoardBounds boardBounds) {
-    List<String> boardWithCells = oldBoard;
-    cells.forEach(c -> {
-      int positionInBoard = getCellPositionInBoard(c, boardBounds);
-      boardWithCells.set(positionInBoard, "|X_");
-    });
-
-    return boardWithCells;
-  }
-
-  private static int getCellPositionInBoard(Cell cell, BoardBounds boardBounds) {
-    int adjustedX = Math.abs(boardBounds.getLeftmostXValue() - cell.x);
-    int adjustedY = Math.abs(boardBounds.getUpperYValue() - cell.y);
-
-    int boardWidth = 2 + boardBounds.getWidth();
-
-    return boardWidth + (adjustedY * boardWidth) + (adjustedX + 1);
+    return String.join("", BoardComponentDrawer.populateBoardWithCells(emptyBoard, cells, boardBounds));
   }
 
 }
