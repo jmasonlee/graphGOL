03f61a8d7a1660620155667b2cdb2162d606397d
Jacqueline Bilston <jmasonlee@gmail.com>
2021-02-21 09:24:55 -0700

    Test LiveCellDrawer

---
+++ src/test/gameOfLife/output/LiveCellDrawerTest.java
@@ -0,0 +1,8 @@
+package gameOfLife.output;
+
+import junit.framework.TestCase;
+
+public class LiveCellDrawerTest extends TestCase {
+  public void testCellsAreAddedToSquaresMatchingCoordinates() {
+  }
+}
