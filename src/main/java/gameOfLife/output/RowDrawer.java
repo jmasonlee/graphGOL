73c3178fb1fb69eaf6a9a6fe58dd28d07c35d5bc
Jacqueline Bilston <jmasonlee@gmail.com>
2021-02-21 20:45:08 -0700

    remove duplicate code

--- src/main/java/gameOfLife/output/RowDrawer.java
+++ src/main/java/gameOfLife/output/RowDrawer.java
@@ -8,17 +8,7 @@ import java.util.List;
 
 public class RowDrawer {
   static List<String> drawRows(Coordinates coordinates) {
-    List<String> board = new ArrayList<>();
-    int boardWidth = coordinates.xCoordinates.size();
-
-    coordinates.yCoordinates.forEach(
-        yCoordinate -> {
-          board.add(yCoordinate);
-          board.addAll(Collections.nCopies(boardWidth, BoardSquare.EMPTY_CELL.getValue()));
-          board.add(BoardSquare.ROW_END.getValue());
-        });
-
-    return board;
+    return drawRows(coordinates, new ArrayList<>());
   }
 
   static List<String> drawRows(Coordinates coordinates, List<Cell> liveCells) {
