238a6514621b29471f69f838b93f93be7cd280ec
Jacqueline Bilston <jmasonlee@gmail.com>
2021-02-23 20:56:43 -0700

    Modify method to use internal var

--- src/main/java/gameOfLife/output/CoordinateSquareDrawer.java
+++ src/main/java/gameOfLife/output/CoordinateSquareDrawer.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 public class CoordinateSquareDrawer {
   protected static void drawCoordinateSquare(List<String> board, String yCoordinate) {
+    String coordinateSquare = "";
     if (yCoordinate.length() == 1) {
-      board.add(BoardSquare.SPACE.getValue() + yCoordinate);
-    } else {
-      board.add(yCoordinate);
+      coordinateSquare = BoardSquare.SPACE.getValue() ;
     }
+
+    coordinateSquare += yCoordinate;
+
+    board.add(coordinateSquare);
   }
 }
