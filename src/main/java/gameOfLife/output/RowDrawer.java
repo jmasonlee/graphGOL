f2d0e9baafe44b211f6a606f1873d5402ddf1ed1
Jacqueline Bilston <jmasonlee@gmail.com>
2021-02-23 20:42:12 -0700

    Extract coordinate square drawing logic

--- src/main/java/gameOfLife/output/RowDrawer.java
+++ src/main/java/gameOfLife/output/RowDrawer.java
@@ -17,15 +17,19 @@ public class RowDrawer {
 
     coordinates.yCoordinates.forEach(
         yCoordinate -> {
-          if (yCoordinate.length() == 1) {
-            board.add(BoardSquare.SPACE.getValue() + yCoordinate);
-          } else {
-            board.add(yCoordinate);
-          }
+          drawCoordinateSquare(board, yCoordinate);
           board.addAll(Collections.nCopies(boardWidth, BoardSquare.EMPTY_CELL.getValue()));
           board.add(BoardSquare.ROW_END.getValue());
         });
 
     return board;
+  }
+
+  private static void drawCoordinateSquare(List<String> board, String yCoordinate) {
+    if (yCoordinate.length() == 1) {
+      board.add(BoardSquare.SPACE.getValue() + yCoordinate);
+    } else {
+      board.add(yCoordinate);
+    }
   }
 }
